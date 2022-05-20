7942f04aeafeac11161bc855e255b5f7f4a0e5ba
Bach Le <lehoangbach7802@gmail.com>
2022-05-20 15:57:03 -0400

    commit tree 7974429e18d6bd8b0dd8b5dd3242380af0a5033d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, driversName) {
+  const findMatching = drivers.filter(name => {
+    return name.toLowerCase() === driversName.toLowerCase()
+  })
+  return findMatching;
+}
+
+console.log(findMatching(drivers, 'bobby'))
+
+function fuzzyMatch(drivers, driversNameLetter) {
+  const fuzzyMatch = drivers.filter(letter => {
+    return letter.substring(0, driversNameLetter.length) === driversNameLetter
+  })
+
+  return fuzzyMatch;
+}
+
+
+console.log(fuzzyMatch(drivers, 'Bo'))
+
+function matchName(array, driverName) {
+    return array.filter(home => home.name === driverName)
+}
